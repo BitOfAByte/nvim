49d7a1f9ff6499946cc6ec688fe4ca4cbaa3bae8
BitOfAByte <hopetobylol@gmail.com>
2024-09-27 18:13:20 +0200

    docker

--- lua/toby/plugins/docker.lua
+++ lua/toby/plugins/docker.lua
@@ -1,14 +1,31 @@
 return {
-	"dgrbrady/nvim-docker",
+	"krisajenkins/telescope-docker.nvim",
+	event = "VeryLazy",
 	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"MunifTanjim/nui.nvim",
+		"nvim-telescope/telescope.nvim",
 	},
+	config = function()
+		require("telescope").load_extension("telescope_docker")
+		require("telescope_docker").setup({})
+	end,
+
+	-- Example keybindings. Adjust these to suit your preferences or remove
+	--   them entirely:
 	keys = {
 		{
-			"<leader>dc",
-			"<cmd>lua require('nvim-docker').containers.list_containers()<cr>",
-			desc = "List Docker containers",
+			"<Leader>dv",
+			":Telescope telescope_docker docker_volumes<CR>",
+			desc = "[D]ocker [V]olumes",
+		},
+		{
+			"<Leader>di",
+			":Telescope telescope_docker docker_images<CR>",
+			desc = "[D]ocker [I]mages",
+		},
+		{
+			"<Leader>dp",
+			":Telescope telescope_docker docker_ps<CR>",
+			desc = "[D]ocker [P]rocesses",
 		},
 	},
 }
